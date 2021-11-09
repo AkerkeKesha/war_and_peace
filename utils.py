30d6c01a8a2cd2b241c440fdb6f08fde98f8ceb5
AkerkeKesha <akerke.kass@gmail.com>
2021-11-09 11:09:06 +0100

    utils: get root dir, get files from certain dirs, make sentence per line

---
+++ utils.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+SENTENCE_PATTERN = r"[A-Z].*?[\.!?]"
+
+
+def get_raw_file(dirname: str, filename: str) -> str:
+    return os.path.join(ROOT_DIR, 'raw_data', dirname, filename)
+
+
+def get_sample_file(filename: str ='sourcetext.txt') -> str:
+    return os.path.join(ROOT_DIR, 'sample', filename)
+
+
+def get_clean_file(filename: str) -> str:
+    return os.path.join(ROOT_DIR, 'data', filename)
+
+
+def make_sentence_per_line(s: str) -> str:
+    for match in re.findall(SENTENCE_PATTERN, s):
+        s = re.sub(match, f'{match}\n', s)
+    return s
