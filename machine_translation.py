cffb665aae6a50bdeb9e89212d7f7349506b5d5a
AkerkeKesha <akerke.kass@gmail.com>
2021-12-01 22:35:43 +0100

    fixed marianMT translation function to run on google colab

--- machine_translation.py
+++ machine_translation.py
@@ -2,6 +2,7 @@ from deep_translator import GoogleTranslator
 from utils import get_clean_file
 from transformers import MarianMTModel, MarianTokenizer
 from typing import List
+import time
 
 MODEL_RU_EN = 'Helsinki-NLP/opus-mt-ru-en'
 TOKENIZER = MarianTokenizer.from_pretrained(MODEL_RU_EN)
@@ -13,9 +14,10 @@ def translate_from(file: str, src_languaguge: str, tgt_language: str) -> str:
     return translated
 
 
-def marian_translate(texts: List[str], model=MODEL, tokenizer=TOKENIZER) -> str:
+def marian_translate(texts: List[str], model=MODEL, tokenizer=TOKENIZER) -> List[str]:
     translated = model.generate(**tokenizer(texts, return_tensors="pt", padding=True))
-    return "\n".join([tokenizer.decode(t, skip_special_tokens=True) for t in translated])
+    translated_sentences = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+    return translated_sentences
 
 
 if __name__ == '__main__':
@@ -25,5 +27,17 @@ if __name__ == '__main__':
         "— А праздник английского посланника?",
         "Нынче середа."], model=MODEL, tokenizer=TOKENIZER)
     print(test)
+    # start_time = time.time()
+    # clean_file_path = get_clean_file('test.txt')
+    # with open(clean_file_path) as fh:
+    #     tolstoy_texts = fh.readlines()
+    # print(f"{len(tolstoy_texts)} sentences to feed")
+    # tolstoy_translated = marian_translate(tolstoy_texts)
+    # print(f"{len(tolstoy_translated)} sentences translated")
+    # for sentence in tolstoy_translated:
+    #     with open('./data/tolstoy_translated.txt', 'a') as fh:
+    #         fh.write(f"{sentence}\n")
+    # print(f"{time.time() - start_time} seconds to translate")
+#     Process finished with exit code 137 (interrupted by signal 9: SIGKILL)
 
 
