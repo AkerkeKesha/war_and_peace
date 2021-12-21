580af60ed2cfccb8a923e6562f8cf02916747c1b
AkerkeKesha <akerke.kass@gmail.com>
2021-12-21 19:25:00 +0100

    added cuda or device to train marianMT for. Locally tested

--- machine_translation.py
+++ machine_translation.py
@@ -1,13 +1,21 @@
-from transformers import MarianMTModel, MarianTokenizer
 from typing import List
+import torch
+from transformers import MarianMTModel, MarianTokenizer
+
+if torch.cuda.is_available():
+  dev = "cuda"
+else:
+  dev = "cpu"
+device = torch.device(dev)
 
 MODEL_RU_EN = 'Helsinki-NLP/opus-mt-ru-en'
 TOKENIZER = MarianTokenizer.from_pretrained(MODEL_RU_EN)
 MODEL = MarianMTModel.from_pretrained(MODEL_RU_EN)
+MODEL.to(device)
 
 
 def marian_translate(texts: List[str], model=MODEL, tokenizer=TOKENIZER) -> List[str]:
-    translated = model.generate(**tokenizer(texts, return_tensors="pt", padding=True))
+    translated = model.generate(**tokenizer(texts, return_tensors="pt", padding=True)).to(device)
     translated_sentences = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
     return translated_sentences
 
@@ -19,6 +27,15 @@ if __name__ == '__main__':
         "— А праздник английского посланника?",
         "Нынче середа."], model=MODEL, tokenizer=TOKENIZER)
     print(test)
+    with open("./data/passage_enemy.txt") as fh:
+        sample = fh.read()
+    tolstoy_translated = marian_translate(sample)
+    for sentence in tolstoy_translated:
+        with open('./data/test_translated_passage_enemy.txt', 'a') as fh:
+            fh.write(f"{sentence}\n")
+    print("done translating")
+
+
 
 
 
