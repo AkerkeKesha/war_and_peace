d37ee909a4a9c0c13cb8f04190071669c31f2e51
AkerkeKesha <akerke.kass@gmail.com>
2022-02-28 19:20:21 +0100

    added small file debugging possibility

--- debug_aligner.py
+++ debug_aligner.py
@@ -1,17 +1,15 @@
 from Bleualign.bleualign.align import Aligner
 
-src_file = './sample/tolstoy.ru.txt'
-target_file = './sample/maude.eng.txt'
-src_to_target = './sample/google-translate.ru.eng.txt'
-output_src_file = './sample/ru.aligned.txt'
-output_target_file = './sample/eng.aligned.txt'
+source = 'sample'
 
 options = {
-    'srcfile': src_file,
-    'targetfile': target_file,
-    'srctotarget': [src_to_target],
-    'output-src': output_src_file,
-    'output-target': output_target_file
+    'srcfile': f'./{source}/tolstoy.ru.txt',
+    'targetfile': f'./{source}/maude.eng.txt',
+    'srctotarget': [
+        f'./{source}/google-translate.ru.eng.txt'
+    ],
+    'output-src': f'./{source}/ru.aligned.txt',
+    'output-target': f'./{source}/eng.aligned.txt'
 }
 
 if __name__ == '__main__':
